a693621bf2f6a60c089ab29248db5d061c7bb7e3
Justin Lewis <j.mapping@gmail.com>
2018-08-16 17:38:55 -0600

    Update RESTAPI.js

--- 1.0/registry-interface/RESTAPI.js
+++ 1.0/registry-interface/RESTAPI.js
@@ -2,6 +2,12 @@
 // GET location 
 //
 
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
+
 // URL
 /location
 
