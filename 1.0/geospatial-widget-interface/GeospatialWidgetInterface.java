789cf13d9bf078774cfdfafd652af6c6ccd1dda9
eotin <eotin@users.noreply.github.com>
2018-08-27 14:31:06 +0200

    Create of GeospatialWidgetInterface.java
    First draft

---
+++ 1.0/geospatial-widget-interface/GeospatialWidgetInterface.java
@@ -0,0 +1,56 @@
+/**
+* Interface to the Geospatial Widget.
+*
+*
+**/
+public interface GeospatialWidgetInterface {
+
+    /**
+    * Access point for getting coordinates of a the current position
+    *
+    **/
+    public JSONObject getLocation();
+	
+	  public JSONObject getDirection();
+    
+    
+    /**
+    * Access points for getting a record of a geographic object with GPS
+    *
+    **/
+    public int startRecordLocation();
+	
+	  public void pauseRecordLocation(int recordId);
+	
+	  public void resumeRecordLocation(int recordId); 
+	
+	  public JSONArray stopRecordLocation(int recordId);
+	
+	 /**
+    * Access points for getting a record of a geographic object without GPS
+	  * 
+    *
+    **/
+	
+	  public int startDrawingLocation();
+	
+	  public JSONObject addDrawingPoint(int drawingId); // Return Point information
+	
+	  public void deleteDrawingPoint(int drawingId, int drawingPointId);
+	
+	  public JSONArray stopDrawingLocation(int drawingId);
+	
+	 /**
+    * Access points for splitting polygons 
+	  * 
+    *
+    **/
+	
+	  public void startSplitPolygon(int polygonId); 
+	
+	  public JSONObject setFirstSplittingPoint(int polygonId);
+	
+	  public JSONObject setSecondSplittingPoint(int polygonId);
+	
+	  public JSONArray stopSplitPolygon(int polygonId);
+}
