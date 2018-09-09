836bd57fc6e1ccdcedf0d5f5ae2447519d50a48d
nmceachen <nathan@mceachen.us>
2018-09-09 15:45:14 -0600

    Update Interface.java

--- 1.0/registry-interface/Interface.java
+++ 1.0/registry-interface/Interface.java
@@ -16,7 +16,7 @@ public interface RegistryInterface {
     * @returns a GeoObject in GeoJSON format with the given uid.
     * @throws
     **/
-    public GeoJSONObject getGeoObject(String uid);  
+    public GeoObject getGeoObject(String uid);  
     
     /**
     * Create a new GeoObject in the Common Geo-Registry
@@ -51,7 +51,7 @@ public interface RegistryInterface {
     * @post 
     *
     * @param parentUid UID of the parent object for which the call fetches children.
-    * @param childrentTypes An array of GeoObjectType names of the types of children GeoObjects to fetch.
+    * @param childrentTypes An array of GeoObjectType names of the types of children GeoObjects to fetch. If blank then return children of all types.
     * @param recursive TRUE if recursive children of the given parent with the given types should be returned, FALSE if only single level children should be returned.  
     * 
     * @returns
@@ -66,7 +66,7 @@ public interface RegistryInterface {
     * @post 
     *
     * @param childUid UID of the child object for which the call fetches parents.
-    * @param parentTypes An array of GeoObjectType names of the types of parent GeoObjects to fetch.
+    * @param parentTypes An array of GeoObjectType names of the types of parent GeoObjects to fetch. If blank then return parents of all types.
     * @param recursive TRUE if recursive parents of the given parent with the given types should be returned, FALSE if only single level parents should be returned.  
     * 
     * @returns
@@ -76,10 +76,13 @@ public interface RegistryInterface {
     
     
     /**
-    * Access points for getting UIDs. The Common Geo-Registry will only accept newly created GeoObjects with a UID that was issued from the Common GeoRegistry.
+    * Get list of valid UIDs for use in creating new GeoObjec The Common Geo-Registry will only accept newly created GeoObjects with a UID that was issued from the Common GeoRegistry.
     *
-    * @precondistions
-    * @postconditions
+    * @pre 
+    * @post 
+    *
+    * @param numberOfUIDs NumberofUIDs that the Common Geo-Registry will issue to the mobile device.
+    *
     * @returns
     * @throws
     **/
@@ -88,25 +91,16 @@ public interface RegistryInterface {
     
     
     /**
-    * Access points for getting UIDs. Commonly used to retrieve an acceptable ID which could be used to persist a new GeoObject.
+    * Return GeoOjectType objects that define the given list of types.
     *
-    * @precondistions
-    * @postconditions
+    * @pre 
+    * @post 
+    *
+    * @param types An array of GeoObjectType names. If blank then all GeoObjectType objects are returned.
+    *
     * @returns
     * @throws
     **/
     GeoObjectType[] getGeoObjectTypes(String[] types);
     
-    
-    
-    
-    /**
-    * Access points for setting location objects. 
-    *
-    * @precondistions
-    * @postconditions
-    * @returns
-    * @throws
-    **/
-    public Boolean setLocation(JSONObject location);
 }
